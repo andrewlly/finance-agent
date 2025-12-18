83d0a98a86270ad2b5290f7d3900f5e023fffde6
billycz8 <billyzhao2002@gmail.com>
2025-12-17 20:13:14 -0800

    header fix

--- tools.py
+++ tools.py
@@ -390,12 +390,10 @@ class ParseHtmlPage(Tool):
         # self.headers = headers
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9",
             "Referer": "https://www.google.com/",
-            "DNT": "1",
-            "Connection": "keep-alive",
-            "Upgrade-Insecure-Requests": "1"
+            "DNT": "1"
         }
 
     @retry_on_429
